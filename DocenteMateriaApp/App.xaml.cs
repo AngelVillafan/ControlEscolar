9bda31b8162867c0e90ac91759ea3bf7a39330bd
Angel Villafán <miguelangel9505@hotmail.com>
2023-03-10 14:51:10 -0600

    Add project files.

---
+++ DocenteMateriaApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DocenteMateriaApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DocenteMateriaApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DocenteMateriaApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
